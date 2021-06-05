5ea904e3a1bb963884d56b555388a64cc01648ad
mixcode <mixcode@users.noreply.github.com>
2021-06-05 20:15:49 +0900

    encoding/json added to pull.sh

--- pull.sh
+++ pull.sh
@@ -8,4 +8,6 @@ git subtree pull --prefix cbccts https://github.com/mixcode/golib-cbccts main --
 # mtrand
 git subtree pull --prefix mtrand https://github.com/mixcode/golib-mtrand master --squash
 
+# encoding/json
+git subtree pull --prefix encoding/json https://github.com/mixcode/golib-json master --squash
 
